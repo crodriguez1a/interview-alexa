2d5e34c635c017aa564a7e299a3244af3ca5b548
Carlos Rodriguez <crodriguez1a@gmail.com>
2018-03-24 12:24:45 -0400

    Handle ask exceptions, Handle Alexa error messages

--- framework/core.py
+++ framework/core.py
@@ -1,7 +1,7 @@
 import subprocess
 import json
 
-def test_utterance(text):
+def test_utterance(text, debug=False):
     """
     Usage:
 
@@ -12,11 +12,27 @@ def test_utterance(text):
     """
     def _outer_wrapper(wrapped_function):
         def _wrapper(*args, **kwargs):
-            ask_cli = subprocess.run(['ask', 'simulate', '--text', text], stdout=subprocess.PIPE)
-            ask_json = json.loads(ask_cli.stdout)
-            ask_says = ask_json['result']['skillExecutionInfo']['invocationResponse']['body']['response']['outputSpeech']['text']
+            commands = ['ask', 'simulate', '--text', text]
+
+            if debug:
+                commands.append('--debug')
+
+            ask_cli = subprocess.run(commands, stdout=subprocess.PIPE)
+
+            # pass through exceptions from ask
+            try:
+                ask_json = json.loads(ask_cli.stdout)
+            except:
+                raise Exception(ask_cli.stdout)
+
+            try:
+                ask_says = ask_json['result']['skillExecutionInfo']['invocationResponse']['body']['response']['outputSpeech']['text']
+            except:
+                ask_says = ask_json['result']['error']['message']
+
             _self = args[0]
             result = wrapped_function(_self, ask_says)
+
             return result
         return _wrapper
     return _outer_wrapper
