d40e53c79f327de8a20f48f2fee426bf764ba3f1
Carlos Rodriguez <crodriguez1a@gmail.com>
2018-03-28 14:22:58 -0400

    A few tips from Aaron L on packaging

---
+++ interview_alexa/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+r"""
+   ____     __               _              ___   __
+  /  _/__  / /____ _____  __(_)__ _    __  / _ | / /____ _____ _
+ _/ // _ \/ __/ -_) __/ |/ / / -_) |/|/ / / __ |/ / -_) \ / _ `/
+/___/_//_/\__/\__/_/  |___/_/\__/|__,__/ /_/ |_/_/\__/_\_\\_,_/ 
+
+"""
+
+__title__ = 'interview-alexa'
+__version__ = '0.1.0'
+__author__ = 'Carlos Rodriguez'
+__license__ = 'BSD 2-Clause'
+__copyright__ = 'Copyright 2018 Carlos Rodriguez'
+
+# Version synonym
+VERSION = __version__
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,16 +1,14 @@
 import unittest
-from interview_alexa import say
-
+from interview_alexa.core import say
 
 class TestInterviewAlexa(unittest.TestCase):
 
     def test_decorator(self):
-
         @say('hello world')
         def foo(self, result):
             self.assertIsNone(result)
 
-    # TODO 
+    # TODO
 
 
 if __name__ == '__main__':
